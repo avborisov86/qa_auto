2fa582a2cd7fb278c563008d269f59a5eb124085
Антон Борисов <avboris@MacBook-Pro-Anton.local>
2021-07-24 18:55:56 +0400

    Added corretions: import in main.py, deleted original json and csv files

--- homework2/main.py
+++ homework2/main.py
@@ -1,12 +1,13 @@
-import homework2
+import books
+import users
 
 if __name__ == '__main__':
 
     # Читаем данные из файла 'csv' и сохраняем в переменную 'books_list'
-    books_list = homework2.read_books(homework2.csv_file)
+    books_list = books.read_books(books.csv_file)
 
     # Читаем данные из файла 'json' и сохраняем в переменную 'users_list'
-    users_list = homework2.read_users(homework2.json_file)
+    users_list = users.read_users(users.json_file)
 
     # Перебираем все книги в списке и распределяем поровну между всеми студентами (кому-то досталось больше на одну)
     while len(books_list) > 0:
@@ -17,4 +18,4 @@ if __name__ == '__main__':
                 user.books.append(books_list.pop(0))
 
     # Делаем encoding данных в формат json и записываем в новый файл result.json
-    homework2.write_users(users_list, homework2.result_json_file)
+    users.write_users(users_list, users.result_json_file)
