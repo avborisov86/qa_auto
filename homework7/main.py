b29a56bb59a0e5444ea6c70363a3d15853b3ae19
Антон Борисов <avboris@MacBook-Pro-Anton.local>
2021-09-24 20:37:18 +0400

    Optional features: command line exec, parsing all files in directory. Plus fixing bugs.

--- homework7/main.py
+++ homework7/main.py
@@ -1,26 +1,18 @@
-from homework7.parser import create_json
-from homework7.parser import get_requests_quantity
-from homework7.parser import find_most_freq_requests
-from homework7.parser import find_slowest_requests
+from parser.parser import create_json
+from parser.parser import get_requests_quantity
+from parser.parser import get_frequent_requests
+from parser.parser import get_slow_requests
+from parser.parser import arg_parser
 
 if __name__ == '__main__':
     # Создаем пустой файл parsing_results.json
     create_json()
 
-    # Считаем общее количество выполненных запросов и дописываем в parsing_results.json
-    get_requests_quantity('/Users/avboris/Desktop/access.log', 'all_requests')
-
-    # Считаем количество выполненных запросов типа GET и дописываем в parsing_results.json
-    get_requests_quantity('/Users/avboris/Desktop/access.log', 'get_requests')
-
-    # Считаем количество выполненных запросов типа POST и дописываем в parsing_results.json
-    get_requests_quantity('/Users/avboris/Desktop/access.log', 'post_requests')
-
-    # Считаем количество выполненных запросов типа HEAD и дописываем в parsing_results.json
-    get_requests_quantity('/Users/avboris/Desktop/access.log', 'head_requests')
+    # Считаем общее количество выполненных запросов/GET запросов/POST запросов/HEAD запросов и дописываем в parsing_results.json
+    get_requests_quantity(arg_parser().path_to_filename)
 
     # Ищем топ 3 IP адресов, с которых было сделано наибольшее количество запросов, и дописываем в parsing_results.json
-    find_most_freq_requests('/Users/avboris/Desktop/access.log')
+    get_frequent_requests(arg_parser().path_to_filename)
 
     # Ищем топ 3х самых долгих запросов и дописываем в parsing_results.json
-    find_slowest_requests('/Users/avboris/Desktop/access.log')
+    get_slow_requests(arg_parser().path_to_filename)
